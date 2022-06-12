0e80b74d863725dec4ea01db83e28dd615f393f3
lokes <lokes@LAPTOP-8UA5N0S6.ht.home>
2022-06-13 01:34:49 +0530

    Added additional Check

--- src/GITTest2.java
+++ src/GITTest2.java
@@ -3,7 +3,7 @@ public class GITTest {
 
 	public static void main(String[] args) {
 		String hungry = "yes";
-		if(hungry == "yes")
+		if(hungry == "yes" || hungry == "Y" || hungry == "y")
 			System.out.println("eat somthing");
 		else {
 			System.out.println("Do Work");
