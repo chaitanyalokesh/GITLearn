f86f7b4a708c806ceac3f852223132b051c60ab0
lokes <lokes@LAPTOP-8UA5N0S6.ht.home>
2022-06-13 01:07:13 +0530

    Committing Test2 to Branch chaitu

---
+++ src/GITTest2.java
@@ -0,0 +1,16 @@
+
+public class GITTest {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.println("eat somthing");
+		else {
+			System.out.println("Do Work");
+
+		}
+		
+
+	}
+
+}
